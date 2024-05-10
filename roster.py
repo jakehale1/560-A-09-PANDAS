3ff03651e334aa06c5d75d63d74407c726cb9253
Jakehale <jakehale@ad.unc.edu>
2024-05-10 11:32:18 -0400

    After watching Panopto Video

--- roster.py
+++ roster.py
@@ -1,10 +1,13 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
-roster = {"RJ Davis", "Armando Bacot", "Seth Trimble"}
+roster = ["Bacot", "Davis", "Cadeau"]
+player = {
+    "Last Name": roster,
+    "First Name": ["Armando", "RJ", "Elliot"],
+    "Height": [83, 72, 73],  # Corrected the height values
+    "Weight": [240, 180, 180]
+}
 
-data = pd.DataFrame(roster, columns=["Player"])
-
-print(data)
-
+data = pd.DataFrame(player)
 
